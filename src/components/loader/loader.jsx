cc81e9d65b0cfe535ba882b8057dfdca9a6481bb
hhelgii <opruska2004@gmail.com>
2023-08-15 21:27:02 +0300

    Update loader.jsx

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -6,7 +6,7 @@ export const Loader = () => {
       height="80"
       width="80"
       radius="9"
-      color="blue"
+      color="white"
       ariaLabel="three-dots-loading"
       wrapperStyle={{}}
       wrapperClassName=""
